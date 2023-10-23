d8cfb6eb266b4dcef4f9c94df7231d4d7c75ae71
Zidan03 <127195755+Zidan03@users.noreply.github.com>
2023-10-23 16:47:09 +0700

    Add files via upload

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Footer from "../components/Footer";
+
+const Contact = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <p>Contact</p>
+      <Footer />
+    </>
+  );
+};
+
+export default Contact;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <p>Home</p>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
